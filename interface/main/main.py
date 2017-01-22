77e8c6e54ee21ab09c35d1f2c006eb5e89f53adf
riordant <riordant@tcd.ie>
2017-01-22 18:12:08 +0000

    Update main.py

--- interface/main/main.py
+++ interface/main/main.py
@@ -1,9 +1,9 @@
 import threading
 from controller import Controller
 from reader import Reader
-
 c = Controller()
 r = Reader()
 threading.Thread(target=c.start).start()
 threading.Thread(target=r.get_distance).start()
+threading.Thread(target=r.get_player).start()
 
