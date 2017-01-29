311563c5c58779aba8fc674e015c6d0f777ed548
sopermaf <sopermaf@tcd.ie>
2017-01-29 19:07:25 +0000

    mutation function added to learning

--- interface/main/learning.py
+++ interface/main/learning.py
@@ -1,9 +1,14 @@
 import random
 import time
 
+from datetime import datetime
+random.seed(datetime.now())
+
 #genetic alphabet
 MIN_NUM_GENES = 8
 MAX_NUM_GENES = 16
+
+NUM_TYPES_GENES = 16
 
 def get_rand_bool():
     return bool(random.getrandbits(1))
@@ -11,21 +16,27 @@ def get_rand_bool():
 def create_runner():
     new_runner = []
     end_creation = False
+    genetic_length = random.randint(MIN_NUM_GENES,MAX_NUM_GENES)
     
-    while end_creation == False and len(new_runner) < MAX_NUM_GENES:
+    for i in range(0, genetic_length):
         new_gene = random.randint(0,15)
         new_runner.append(new_gene)
         
-        if len(new_runner) < MIN_NUM_GENES:
-            end_creation = False
-        else:
-            end_creation = get_rand_bool()
-        
     return new_runner
 
-start = time.clock()
-
-for x in range(0,500000):
-    y = x
+#changes 1 gene in the genetic sequence
+def mutation(genetic_sequence):
+    size = len(genetic_sequence)
+    gene_num = random.randint(0,size-1)
+    new_mutation = random.randint(0,NUM_TYPES_GENES-1)
+    #print (gene_num, new_mutation)
     
-print(time.clock() - start)
+    #ensure the mutation cannnot be the same as before
+    while new_mutation == genetic_sequence[gene_num]:
+       new_mutation = random.randint(0,NUM_TYPES_GENES-1)
+    
+    new_gen = list(genetic_sequence)
+    new_gen[gene_num] = new_mutation
+    
+    return new_gen
+
