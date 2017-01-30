e803485b93e8363fad9fb2639f1840a4441657bc
riordant <riordant@tcd.ie>
2017-01-30 19:24:58 +0000

    Update learning.py

--- interface/main/learning.py
+++ interface/main/learning.py
@@ -1,29 +1,75 @@
+from dependancies import *
+import controller
 import random
-import time
+from operator import itemgetter
 
-from datetime import datetime
-random.seed(datetime.now())
+#initial_file_write = open("initial.txt", 'w')
+initial_file_read = open("initial.txt", 'r')
+final_file_write = open("final.text", "w")
 
-#genetic alphabet
-MIN_NUM_GENES = 8
-MAX_NUM_GENES = 16
+row_length = 5
+col_length = 6
+total_length = row_length * col_length
 
-NUM_TYPES_GENES = 16
+torus = [[['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]], 
+         [['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]], 
+         [['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]], 
+         [['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]], 
+         [['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]], 
+         [['NNMJBACDOHIPIMKDIC', 2.4], ['KMNHMMEAOH', 2.8], ['LIDNJBDDHGAEN', 2.4], ['PGJHDLCIGNAMMJ', 2.2], ['HMAKOHAEGDGPDOMC', 2.4]]]                           
 
-def get_rand_bool():
-    return bool(random.getrandbits(1))
+def split_list(list):
+    result = []
+    start = 0
+    end = row_length
+    while(end<=len(list)):
+        row = list[start:end]
+        result.append(row)
+        start+=row_length
+        end+=row_length
+    return result
+       
+def int_to_alphabet(runner):
+    string = ""
+    for int in runner:
+        string += alphabet_string[int]
+    return string
 
 def create_runner():
     new_runner = []
     end_creation = False
-    genetic_length = random.randint(MIN_NUM_GENES,MAX_NUM_GENES)
-    
+    genetic_length = random.randint(MIN_NUM_GENES,MAX_NUM_GENES)  
     for i in range(0, genetic_length):
         new_gene = random.randint(0,15)
         new_runner.append(new_gene)
         
-    return new_runner
+    return new_runner           
 
+def get_best_neighbour(torus, x, y):
+    neighbour_list = []  
+    index_list = [] 
+    #gets neighbour torus positions 
+    for neighbour_x in range(-1,2):
+        for neighbour_y in range(-1,2):
+            if(not(neighbour_x==0 and neighbour_y==0)):
+                x_pos = ((x+neighbour_x)+col_length)%col_length
+                y_pos = ((y+neighbour_y)+row_length)%row_length
+                neighbour_list.append(torus[x_pos][y_pos])
+                index_list.append([x_pos,y_pos])
+
+    #creates a list of fitness values for sorting
+    fitness_list = []
+    for i in range(0,len(neighbour_list)):
+        fitness_list.append(neighbour_list[i][1])
+    
+    
+    #if multiple best neighbours, chose one at random.
+    indexes_of_best_neighbour = [i for i, j in enumerate(fitness_list) if j==max(fitness_list)]     
+    if(len(indexes_of_best_neighbour)==1): best_neighbour = indexes_of_best_neighbour[0]
+    else: best_neighbour = indexes_of_best_neighbour[random.randint(0,len(indexes_of_best_neighbour)-1)]
+    #finally, convert best neighbour to index relative to x and y.
+    return index_list[best_neighbour]
+      
 #changes 1 gene in the genetic sequence
 def mutation(genetic_sequence):
     size = len(genetic_sequence)
@@ -38,7 +84,7 @@ def mutation(genetic_sequence):
     new_gen = list(genetic_sequence)
     new_gen[gene_num] = new_mutation
     
-    return new_gen
+    return str(new_gen)
 
 #returns an list of lists, with x[0] = A_splice, x[1] = B_splice
 def single_splice(genetic_A, genetic_B):
@@ -58,5 +104,81 @@ def single_splice(genetic_A, genetic_B):
     cl = []
     cl.append(new1)
     cl.append(new2)
+    cl[0] = str(cl[0])
+    cl[1] = str(cl[1])
     return cl
+
+def convert_string_to_int_alphabet(string):
+    int = []
+    string = list(string)
+    for letter in string:    
+        int.append(alphabet_string.index(letter))    
+    return int
     
+'''  To train first set of runners.
+def initial():
+    c = controller.Controller()
+    c.start()
+    num_runners = 0
+    while(num_runners<MAX_INITIAL_RUNNERS):
+        runner = create_runner()
+        fitness_result = c.runner_fitness_test(runner)
+        runner_str = int_to_alphabet(runner)
+        initial_file_write.write(runner_str + " " + str(fitness_result) + "\n")
+        c.check_for_end()
+        num_runners+=1
+'''
+
+def post_process(): #take best 30 from runners 
+    with initial_file_read as f:
+        content = f.readlines()
+        content = [x.strip() for x in content] 
+    for i,line in enumerate(content):
+        content[i] = content[i].split(" ")
+        try: content[i][1] = float(content[i][1]) #divide into numbers only
+        except ValueError: content[i][1] = 0
+    best_content = sorted(content, key=itemgetter(1))
+    return best_content[len(best_content)-30:]
+
+def final_process(): 
+    #best_content = post_process()
+    #best_content = random.sample(best_content, len(best_content)) #randomize values
+    #torus = split_list(best_content) #split into 5x6
+    c = controller.Controller()
+    c.start()
+    iterations = 0
+    while(True):
+        for i in range(0,col_length):
+            for j in range(0,row_length):
+                #perform run
+                print("indexes: " + str(i) + " " + str(j))
+                print(str(torus[i][j][0]))
+                genetic = convert_string_to_int_alphabet(str(torus[i][j][0]))
+                new_fitness = c.runner_fitness_test(genetic)
+                if(new_fitness is None): new_fitness = 0
+                torus[i][j][1] = new_fitness
+                #get best neighbour
+                best_neighbour = get_best_neighbour(torus,i,j)
+                #splice
+                
+                do_splice = single_splice(list(torus[i][j][0]),list(torus[best_neighbour[0]][best_neighbour[1]][0]))
+                torus[i][j][0] = do_splice[0]
+                torus[best_neighbour[0]][best_neighbour[1]][0] = do_splice[1]
+                #restart game
+                c.check_for_end()
+
+        #mutate    
+        for i in range(0,col_length):
+            for j in range(0,row_length):
+                torus[i,j][0] = mutate(list(torus[i,j][0]))
+        
+        #write all to file every torus completion
+        final_file_write.write("Iteration: " + str(iteration) + "\n")
+        for i in range(0,col_length):
+            for j in range(0,row_length):
+                final_file_write.write(torus[i][j][0] + " " + torus[i][j][1]  + "\n")
+        final_file_write.write("\n")
+                    
+if __name__ == "__main__": 
+    final_process()
+      
